f104e4ab23ff5d7a0528d2f1e39ddc6e21f21cc2
Noam-Elron <noam.elron.college@gmail.com>
2023-05-04 04:51:51 +0300

    added class desc, repr and accepted file type extensions

--- fileprompter.py
+++ fileprompter.py
@@ -4,18 +4,37 @@ from tkinter import filedialog as fd
 import os 
 
 class FileDialogWindow:
+    """
+    Opens a file dialog window
+
+    Parameters:
+        Optional: 
+            Directory -- Either simply directory name if directory in cwd or full path if root is set to True.
+            Root -- Boolean, True if directory exists outside cwd.
+
+    Returns:
+        Returns a FileDialogWindow instance, instance contains filename instance variable.
+    
+    Raises:
+        Nothing
+    """
+
+    # TODO - Create failsafes incase direcory entered incorrectly or root used incorrectly.
     root = tk.Tk()
     root.title('Tkinter Open File Dialog')
     root.resizable(False, False)
     root.geometry('300x150')
-    def __init__(self, directory=None):
+    def __init__(self, directory=None, root=False):
         self.filename = None
         self.directory = directory
+        self.root = root
         self.path = self.get_path()
         self.file_dialog()
 
     def get_path(self):
-        if self.directory != None:
+        if self.directory != None and self.root != False:
+            path = self.directory
+        elif self.directory != None:
             path = os.path.join(os.getcwd(), self.directory)
         else:
             path = os.getcwd()
@@ -27,7 +46,7 @@ class FileDialogWindow:
 
         def select_files():
             filetypes = (
-                ('text files', '*.jpg *.png'),
+                ('text files', '*.jpg *.png *.jpeg'),
                 ('All files', '*.*')
             )
             # Normalize os path so that it auto configures \ for windows and // for linux
@@ -53,3 +72,6 @@ class FileDialogWindow:
         
         # run the application
         self.root.mainloop() 
+
+    def __repr__(self):
+        return f"{__class__}({self.directory}, {self.root})"
