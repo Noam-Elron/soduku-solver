91e0a40abd446312e050160944351d7b575184f6
Noam-Elron <noam.elron.college@gmail.com>
2024-10-18 02:44:13 +0300

    Another seperation of functions from utils. New function Configure threshold is used to more easily determine ideal parameters for adaptive thresholding.

--- src/util/manual_image_viewing.py
+++ src/util/manual_image_viewing.py
@@ -1,29 +1,23 @@
-import numpy as np
-import matplotlib as mp
 import matplotlib.pyplot as plt
 import cv2 as cv
 from sudoku_logic.sudoku_scanner import SudokuImage
-from .utils import return_dataset_images
 from .imgplotting import image_show_matplotlib, multi_image_show_matplotlib
-from matplotlib.widgets import Button, Slider
+from matplotlib.widgets import Slider
 
-def look_at_dataset_warped(directory: str, reverse: bool = False):
-    
+def look_at_dataset_warped(images: str):
     """
-    Shows every image's warped perspective that is inside the given directory
+    Shows every image's warped perspective that is inside the given directory. Assumes image in image#number.file_extension format.
 
     Parameters:
-        directory(str): directory that contains images whose names start in image followed by a number
-
+        images(str): list of paths to images.
     Returns:
         Nothing
     
     Raises:
         Nothing
     """
-    files = return_dataset_images(directory, reverse)
-    for file in files:
-        img = SudokuImage(file)
+    for image in images:
+        img = SudokuImage(image)
         print(img.shortened_filename)
         try:
             board, board_binary, board_size = img.find_board_location()
@@ -33,37 +27,40 @@ def look_at_dataset_warped(directory: str, reverse: bool = False):
             print(f"{img.shortened_filename}-problematic")
             continue
 
-def configure_threshold(directory: str, reverse: bool = False):
+def configure_threshold(images: str, default_blocksize: int = 53, default_c: int = 7):
 
     """
-    Creates a window with multiple images
+    Creates a window with multiple images for manual adaptive threshold configuration. 
+    Assumes image in image#number.file_extension format.
 
     Parameters:
-        directory(str): directory that contains images whose names start in image followed by a number
-
+        images(str): list of paths to images.
     Returns:
         Nothing
-    
     Raises:
         Nothing
     """
-    files = return_dataset_images(directory, reverse)
-    for file in files:
-        img = SudokuImage(file)
+
+    for image in images:
+        img = SudokuImage(image, default_blocksize, default_c)
+        # Need to convert color space as opencv reads images as BGR, but matplotlib wants RGB images. Without changing color space colors are wonky.
+        img.img = cv.cvtColor(img.img, cv.COLOR_BGR2RGB)
+        img_binarized = img.binarize_image(img.img.copy())
         _, warped_board_binary, _ = img.find_board_location()
 
         try:
             fig, axes = plt.subplots(ncols=2)
-            axes_img1 = axes[0].imshow(img.img, cmap='gray', interpolation='none')
+            fig.canvas.manager.full_screen_toggle()
+            axes_img1 = axes[0].imshow(img_binarized, cmap='gray', interpolation='none')
             axes_img2 = axes[1].imshow(warped_board_binary, cmap='gray', interpolation='none')
             axes_imgs = [axes_img1, axes_img2]
             plt.subplots_adjust(bottom=0.35)
 
             cfreq = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-            c_slider = Slider(ax=cfreq, label='c', valmin=0, valmax=40, valinit=7, valstep=1,)
+            c_slider = Slider(ax=cfreq, label='c', valmin=0, valmax=40, valinit=default_c, valstep=1,)
 
             blocksizefreq = fig.add_axes([0.25, 0.3, 0.65, 0.03])
-            bs_slider = Slider(ax=blocksizefreq, label='Blocksize', valmin=1, valmax=300, valinit=23, valstep=[i for i in range(1, 300, 2)],)
+            bs_slider = Slider(ax=blocksizefreq, label='Blocksize', valmin=1, valmax=300, valinit=default_blocksize, valstep=[i for i in range(1, 300, 2)],)
             axes[0].set_title(img.shortened_filename)
             axes[0].set_xticks([])
             axes[0].set_yticks([])
@@ -77,8 +74,8 @@ def configure_threshold(directory: str, reverse: bool = False):
             plt.show()
         except Exception as e:
             # Doesn't raise an Exception in order to debug bad images faster.
-            raise e
             print(f"{img.shortened_filename}-problematic")
+            print(e)
             continue
             
 def update_c(img, axes_imgs, val):
@@ -91,5 +88,4 @@ def update_bs(img, axes_imgs, val):
     axes_imgs[1].set_data(img.find_board_location()[1])
 
 
-# register the update function with each slider
 
