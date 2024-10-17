9a25b81bfa7f32a18c30315ddbd2ccd87d5d4501
Noam-Elron <noam.elron.college@gmail.com>
2024-10-18 02:34:10 +0300

    Restructured utils file. in Imgplotting functions changed color space to RGB for matplotlib, as opencv reads img as BGR.

--- src/util/imgplotting.py
+++ src/util/imgplotting.py
@@ -7,6 +7,7 @@ import cv2 as cv
 
 
 def image_show_matplotlib(image, title="Image"):
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     fig = plt.figure()
     plt.imshow(image, cmap='gray', interpolation='none')
     plt.title(title)
@@ -20,6 +21,7 @@ def multi_image_show_matplotlib(images, num_images, num_in_row):
     """
     Show multiple images in same matplotlib figure. 
     """
+    images = [cv.cvtColor(image, cv.COLOR_BGR2RGB) for image in images]
     rows = math.ceil(num_images/num_in_row)
     fig, axis = plt.subplots(nrows=rows, ncols=num_in_row, figsize=(12, 14))
     axis = axis.flatten()
