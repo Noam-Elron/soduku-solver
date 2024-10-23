c004b9c108d944659ed2ef253991d09d2bfd26cb
Noam-Elron <noam.elron.college@gmail.com>
2024-10-23 08:34:25 +0300

    Removed unused funcs. Remaking split cells and extract digits

--- src/sudoku_logic/sudoku_scanner.py
+++ src/sudoku_logic/sudoku_scanner.py
@@ -2,7 +2,6 @@ import cv2 as cv
 import numpy as np
 import re
 import os
-from util.fileprompter import FileDialogWindow
 from util.imgplotting import multi_image_show_matplotlib, image_show_matplotlib
 from util.predict import predict_all, prediction_show_matplotlib
 
@@ -27,8 +26,9 @@ class SudokuImage:
         Returns:
             str: String format of actual board
         """
-        
-        cells = self.return_all_cells(debug, binary=True)
+        warped_board, warped_board_binary, board_size = self.find_board_location(debug)
+
+        cells = self.split_image_to_cells(warped_board_binary, board_size)
         
         digits, digit_positions = self.extract_digits(cells)
         
@@ -48,24 +48,6 @@ class SudokuImage:
 
         #return grid_stringified
 
-    def return_all_cells(self, debug=False, binary=True):
-        """
-        Returns an array of images of all the cells in the board
-
-        Args:
-            binary (bool, optional): Flag that determines if returns cells will be binarized. Defaults to True.
-
-        Returns:
-            List[images]: List of images of cells
-        """
-        warped_board, warped_board_binary, board_size = self.find_board_location(debug)
-        cells = []
-        if binary==True:
-            cells = self.split_image_to_cells(warped_board_binary, board_size)
-        else:
-            warped_board = cv.cvtColor(warped_board, cv.COLOR_BGR2GRAY)
-            cells = self.split_image_to_cells(warped_board, board_size)
-        return cells
         
     def find_board_location(self, debug=False):
         """
@@ -125,7 +107,9 @@ class SudokuImage:
             multi_image_show_matplotlib([self.img, cv.cvtColor(self.img, cv.COLOR_BGR2GRAY), image_binary, img_for_contour, warped, warped_binary], 6, 2)
 
         return warped, warped_binary, board_size
-    
+
+
+
     def warp_perspective(self, image, image_binary, location, size):
         """
         Apply a perspective warp - a.k.a creating a new image with a warped perspective so that 
@@ -165,23 +149,10 @@ class SudokuImage:
             ndarray[Cells]: Numpy arrays of the cells of the sudoku board.
         """
         cells = []
-        cell_width, cell_height = board_size//9, board_size//9
-        
-        for row in range(9):
-            for column in range(9):
-                top_left_x, top_left_y, bottom_right_x, bottom_right_y = column*cell_width, row*cell_height, (column + 1)*cell_width, (row + 1)*cell_height
 
-                # First dimension corresponds to rows(y), second to columns(x)
-                cell = warped_image[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
-                cells.append(cell)
-
+        return cells
         
-        kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
-        for i in range(len(cells)):
-            cells[i] = cv.bitwise_not(cells[i])
-            cells[i] = cv.morphologyEx(cells[i], cv.MORPH_OPEN, kernel)
         
-        return np.asarray(cells)
 
     def extract_digits(self, cells):
         """Extracts digits from sudoku cells
@@ -193,28 +164,10 @@ class SudokuImage:
             List[Images]: List of images of the extracted digits
         """
         digits = []
-        positions = []
-        for i, cell in enumerate(cells):
-            cell = cv.copyMakeBorder(cell, 5, 5, 5, 5, cv.BORDER_CONSTANT, 0)
-            
-            contours, _ = cv.findContours(cell, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-            for contour in contours:
-                imgx, imgy, img_width, img_height = cv.boundingRect(contour)
-                # Img_height needs to be atleast 0.75 
-                #print(f"Height: {img_width}, Width: {img_height}, Cell Shape: {cell.shape}, Cell #{i}")
-                #print(f"not img_height>3*img_width: {not(img_height>3*img_width)}, img_width > 0.3*img_height: {img_width > 0.3*img_height}, img_width*img_height > (0.05*(cell.shape[0] * cell.shape[1])): {img_width*img_height > (0.05*(cell.shape[0] * cell.shape[1]))}, img_width*img_height < (0.6*(cell.shape[0] * cell.shape[1]): {img_width*img_height < (0.6*(cell.shape[0] * cell.shape[1]))}")
-                if not(img_height>3*img_width) and img_width > 0.3*img_height and img_width*img_height > (0.05*(cell.shape[0] * cell.shape[1])) and img_width*img_height < (0.6*(cell.shape[0] * cell.shape[1])):
-                    digit = cell[imgy:imgy+img_height, imgx:imgx+img_width]
-                    digit = cv.resize(digit, (18,18))
-                    digit = cv.copyMakeBorder(digit, 5, 5, 5, 5, cv.BORDER_CONSTANT, 0)
-                    #cv.rectangle(cell, (imgx,imgy), (imgx+img_width,imgy+img_height), (0, 255, 0), 3)
-                    #image_show_matplotlib(digit, f"Cell #{i}")
-                    digits.append(digit)
-                    positions.append(i)
-                    break
+        
 
         digits = np.reshape(digits, (-1, 28, 28, 1))
-        return digits, positions
+        return digits
 
 
     def reorder(self, board_location):
@@ -245,9 +198,6 @@ class SudokuImage:
 
         return np.array([top_left, top_right, bottom_left, bottom_right])
     
-
-
-
     def predict_board(self, digits, digit_positions):
         predictions = predict_all(digits, digit_positions)
         grid = np.zeros(81, int)
@@ -274,112 +224,4 @@ class SudokuImage:
         img_copy = cv.adaptiveThreshold(img_copy,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, self.blocksize, self.c)
         return img_copy
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def extract_digits2(self):
-        """
-        Gets Cells based on contours, havent thought of way yet to find positions of cells though. So far failed attempt as unneccesary noise found...
-        """
-        
-        warped_board, warped_board_binary, board_size = self.find_board_location()
-
-        # Look at all contours in warped board, with the entire hierarchy tree.
-        contours, hierarchies = cv.findContours(warped_board_binary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        # For some reason hierarchies array is given wrapped inside an unnecessary dimension
-        hierarchies = hierarchies[0]
-
-        # Cant store numpy arrays as keys so need to use index as key.
-        levels = list(map(lambda contour_index: self.get_hierarchy_level(hierarchies, contour_index), range(len(contours))))
-
-        # Mapping each contour to its hierarchy level, going to use this to remove contours whose hierarchy level is 0, aka external contours aka the borders of the sudoku grid.
-        # Mapping itself useful to sort based on hierarchy level and also filtering later
-        # Hierarchy only useful for looking at the digits parent which will probably be the cell border theyre in
-        contour_hierarchy_level_mapping = list(zip(contours, hierarchies, levels))
-        contour_hierarchy_level_mapping = sorted(contour_hierarchy_level_mapping, key = lambda mapping: mapping[2])
-        sorted_contours = [contour_mapping[0] for contour_mapping in contour_hierarchy_level_mapping]
-        sorted_hierarchies = [hierarchy_mapping[1] for hierarchy_mapping in contour_hierarchy_level_mapping]
-
-        # Removing all from list all contours whose hierarchy level is 0 aka external contours aka borders of sudoku grid. 
-        filtered_mapping = list(filter(lambda mapping: mapping[2] > 0, contour_hierarchy_level_mapping))
-        filtered_contours = [contour_mapping[0] for contour_mapping in filtered_mapping]
-        sorted_hierarchies = [hierarchy_mapping[1] for hierarchy_mapping in filtered_mapping]
-        
-        # Useful for diagnostic image testing.
-        color_mapping = {0: (255, 0, 0), 1: (0, 255, 0), 2: (0, 0, 255), 3: (0, 200, 200), 4: (255, 255, 0), 5: (127, 0 , 255), 6: (128, 128, 128)}
-
-        # Applying boundingRect on filtered_contours returns an array where last two indices are img_width and img_height. Map is iterable thus can iterate over all arrays
-        # obtained from operation and thus easily find each contours area
-        contour_areas = [area_params[2]*area_params[3] for area_params in map(cv.boundingRect, filtered_contours) if area_params[2]*area_params[3] < 2500]
-        
-        # Reason why we wanted the contour areas is to find the mean value, we're assuming that 1. the area of an average digit is greater than the area of random dots of noise and 
-        # 2. that theres not enough noise s.t that their accumulated sum affects the mean to such a degree that the results are truly skewed.
-        mean = np.mean(contour_areas)
-        """
-        for i, contour in enumerate(sorted_contours):
-            imgx, imgy, img_width, img_height = cv.boundingRect(contour)
-            print(f"Area: {img_width*img_height}, Greater than half of mean: {img_width*img_height > 0.5*mean}, Mean: {mean}")
-            warped_board = cv.drawContours(warped_board, sorted_contours, i, color_mapping[contour_hierarchy_level_mapping[i][2]], 5, cv.LINE_4) 
-        image_show_matplotlib(warped_board)
-        """
-        for i, contour in enumerate(filtered_contours):
-            imgx, imgy, img_width, img_height = cv.boundingRect(contour) # Cant simply replace with contour_areas[i] as imgx, imgy, img_width and img_height needed for extracting the digits later.
-            
-            if (0.5*mean) <= (img_width*img_height) <= (3*mean) and (img_height/img_width <= 3) and (img_width/img_height <= 3):
-                contour_hierarchy = sorted_hierarchies[i]
-                parentx, parenty, parent_width, parent_height = cv.boundingRect(contours[contour_hierarchy[3]])
-                cv.rectangle(warped_board, (imgx,imgy), (imgx+img_width,imgy+img_height), (0, 255, 0), 3)
-                #cv.rectangle(warped_board, (parentx,parenty), (parentx+parent_width,parenty+parent_height), (0, 0, 255), 3)
-        
-        image_show_matplotlib(warped_board)
-        
-
-    @staticmethod
-    def get_hierarchy_level(hierarchies, contour_index):
-        level = 0
-
-        while hierarchies[contour_index][3] != -1:
-            contour_index = hierarchies[contour_index, 3]
-            level += 1
-        return level
-
-
-
-
-
-
-
-
-
-
-    def output_cells(self, cells, num_cells, directory):
-        path = os.path.join(os.getcwd(), f"{directory}")
-        for i in range(num_cells):
-            cell = cv.resize(cells[i], (28, 28))
-            cell_path = os.path.join(path, f'{self.shortened_filename}-{i}.jpg')
-            cv.imwrite(cell_path, cell)
-
-
-
-def main():
-    win = FileDialogWindow("dataset")
-    image = SudokuImage(win.filename)    
-    image.find_board_location
-    grid = image.return_board()
     
-
-
-    
-if __name__ == "__main__":
-    main()
-
